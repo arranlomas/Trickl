ed42fd70c02ca7ca03d3ca9f2e7ffbde69185b9d
arran <lomasarran1993@googlemail.com>
2017-11-12 20:52:35 +1000

    run move folders in seperate thread

--- app/src/main/java/com/shwifty/tex/views/settings/mvi/InteractorSideEffects.kt
+++ app/src/main/java/com/shwifty/tex/views/settings/mvi/InteractorSideEffects.kt
@@ -4,27 +4,28 @@ import android.content.Context
 import android.support.annotation.StringRes
 import com.shwifty.tex.utils.ValidateChangeWorkingDirectoryResult
 import com.shwifty.tex.utils.validateWorkingDirectoryCanBeChanged
-import rx.Emitter
-import rx.Observable
+import rx.subjects.PublishSubject
 import java.io.File
 
 /**
  * Created by arran on 11/11/2017.
  */
-fun handleChangeDirectory(event: SettingsIntents.UpdateWorkingDirectory, saveWorkingDirectory: (Context, File) -> Unit): Observable<ChangeWorkingDirectoryResult> {
-    return Observable.create<ChangeWorkingDirectoryResult>({
+fun handleChangeDirectory(event: SettingsIntents.UpdateWorkingDirectory, saveWorkingDirectory: (Context, File) -> Unit): PublishSubject<ChangeWorkingDirectoryResult> {
+    val subject: PublishSubject<ChangeWorkingDirectoryResult> = PublishSubject.create()
+    Thread {
         val isValid = event.newDirectory.validateWorkingDirectoryCanBeChanged(event.previousDirectory)
         if (isValid is ValidateChangeWorkingDirectoryResult.Error) {
-            it.onNext(ChangeWorkingDirectoryResult.CannotChange(isValid.messageRes))
+            subject.onNext(ChangeWorkingDirectoryResult.CannotChange(isValid.messageRes))
         } else {
             saveWorkingDirectory.invoke(event.context, event.newDirectory)
             if (event.moveFiles) {
                 event.previousDirectory.copyRecursively(event.newDirectory, overwrite = true)
                 event.previousDirectory.deleteRecursively()
             }
-            it.onNext(ChangeWorkingDirectoryResult.Changed())
+            subject.onNext(ChangeWorkingDirectoryResult.Changed())
         }
-    }, Emitter.BackpressureMode.BUFFER)
+    }.start()
+    return subject
 }
 
 sealed class ChangeWorkingDirectoryResult {
